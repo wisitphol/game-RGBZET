049ea8cded9993ef0d20833ba30d060b0a72a386
wisitphol <wisitphon1@gmail.com>
2023-08-03 14:03:25 +0700

    update firebase script (ake)

---
+++ RGBZET/Assets/Scripts/UIManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    [SerializeField]
+    private GameObject loginPanel;
+
+    [SerializeField]
+    private GameObject registrationPanel;
+
+    [SerializeField]
+    private GameObject gamePanel;
+
+    [SerializeField]
+    private GameObject SearchRoomPanel;
+
+    [SerializeField]
+    private GameObject CreateRoomPanel;
+
+    [SerializeField]
+    private GameObject UIRoomPanel;
+
+    [SerializeField]
+    private GameObject UIGamePanel;
+    
+
+    private void Awake()
+    {
+        CreateInstance();
+    }
+
+    private void CreateInstance()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    private void ClearUI(){
+        registrationPanel.SetActive(false);
+        loginPanel.SetActive(false);
+        gamePanel.SetActive(false);
+        SearchRoomPanel.SetActive(false);
+        CreateRoomPanel.SetActive(false);
+        UIRoomPanel.SetActive(false);
+        UIGamePanel.SetActive(false);
+    }
+
+    public void OpenLoginPanel()
+    {
+        ClearUI();
+        loginPanel.SetActive(true);
+    }
+
+    public void OpenRegistrationPanel()
+    {
+        ClearUI();
+        registrationPanel.SetActive(true);
+    }
+
+    public void OpenGamePanel(){
+        ClearUI();
+        gamePanel.SetActive(true);
+    }
+
+    public void OpenSearchRoomPanel()
+    {
+        ClearUI();
+        SearchRoomPanel.SetActive(true); 
+    }
+
+    public void OpenCreateRoomPanel()
+    {
+        ClearUI();
+        CreateRoomPanel.SetActive(true); 
+    }
+
+    public void OpenUIRoomPanel()
+    {
+        ClearUI();
+        UIRoomPanel.SetActive(true); 
+    }
+
+    public void OpenUIGamePanel()
+    {
+        ClearUI();
+        UIGamePanel.SetActive(true); 
+    }
+}
---
+++ RGBZET/Assets/Scripts/firebase script/FirebaseConfig.cs
@@ -0,0 +1,11 @@
+// using UnityEngine;
+
+// public static class FirebaseConfig
+// {
+//     public static string FirebaseApiKey = "AIzaSyClOT3VvZeq7vHl0jZU0Z-olV_YHr8Lv1Q";
+//     public static string FirebaseAuthDomain = "https://fir-auth-ad20e.firebaseapp.com/";
+//     public static string FirebaseDatabaseURL = "https://fir-auth-ad20e-default-rtdb.asia-southeast1.firebasedatabase.app/";
+//     public static string FirebaseProjectId = "fir-auth-ad20e";
+//     public static string FirebaseStorageBucket = "";
+//     public static string FirebaseMessagingSenderId = "";
+// }
