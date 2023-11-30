3dd427b0cf0886eaf312327f276e0bea9c35e3ff
wisitphol <wisitphon1@gmail.com>
2023-11-30 15:58:55 +0700

    make card (ake)

---
+++ RGBZET/Assets/Scripts/carddata.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carddata : MonoBehaviour
+{
+    public string letter;
+    public string color;
+    public string font;
+    public int scale;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
