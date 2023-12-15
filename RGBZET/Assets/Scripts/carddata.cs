fb39ae240cc7c9b035e4b6f43c3b0ac7c64eea35
wisitphol <wisitphon1@gmail.com>
2023-12-15 10:12:10 +0700

    update card 4 (ake)

--- RGBZET/Assets/Scripts/carddata.cs
+++ RGBZET/Assets/Scripts/carddata.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class carddata : MonoBehaviour
+public class Carddata : MonoBehaviour
 {
     
     public enum LetterType
