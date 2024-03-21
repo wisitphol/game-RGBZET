575a556b1873078e93cb49fc17a0c6360008d99b
wisitphol <wisitphon1@gmail.com>
2024-03-21 18:15:25 +0700

    update artcard (ake)

--- RGBZET/Assets/Scripts/carddata.cs
+++ RGBZET/Assets/Scripts/carddata.cs
@@ -12,33 +12,35 @@ public class CardData : MonoBehaviour
 
   void Awake()
   {
-      cardList.Add(new Card(0  ,"R" ,"Red"   ,"One" ,"Merk",1 ,Resources.Load<Sprite>("n1") ));
-      cardList.Add(new Card(1  ,"R" ,"Red"   ,"One" ,"Game",1 ,Resources.Load<Sprite>("n2") ));
-      cardList.Add(new Card(2  ,"R" ,"Red"   ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("n3") ));
-      cardList.Add(new Card(3  ,"R" ,"Green" ,"One" ,"Merk",1 ,Resources.Load<Sprite>("n4") ));
-      cardList.Add(new Card(4  ,"R" ,"Green" ,"One" ,"Game",1 ,Resources.Load<Sprite>("n5") ));
-      cardList.Add(new Card(5  ,"R" ,"Green" ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("n6") ));
-      cardList.Add(new Card(6  ,"R" ,"Blue"  ,"One" ,"Merk",1 ,Resources.Load<Sprite>("n7") ));
-      cardList.Add(new Card(7  ,"R" ,"Blue"  ,"One" ,"Game",1 ,Resources.Load<Sprite>("n8") ));
-      cardList.Add(new Card(8  ,"R" ,"Blue"  ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("n9") ));
-      cardList.Add(new Card(9  ,"G" ,"Red"   ,"One" ,"Merk",1 ,Resources.Load<Sprite>("n10") ));
-      cardList.Add(new Card(10 ,"G" ,"Red"   ,"One" ,"Game",1 ,Resources.Load<Sprite>("n11") ));
-      cardList.Add(new Card(11 ,"G" ,"Red"   ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("n12") ));
-      cardList.Add(new Card(12 ,"G" ,"Green" ,"One" ,"Merk",1 ,Resources.Load<Sprite>("n13") ));
-      cardList.Add(new Card(13 ,"G" ,"Green" ,"One" ,"Game",1 ,Resources.Load<Sprite>("n14") ));
-      cardList.Add(new Card(14 ,"G" ,"Green" ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("n15") ));
-      cardList.Add(new Card(15 ,"G" ,"Blue"  ,"One" ,"Merk",1 ,Resources.Load<Sprite>("n16") ));
-      cardList.Add(new Card(16 ,"G" ,"Blue"  ,"One" ,"Game",1 ,Resources.Load<Sprite>("n17") ));
-      cardList.Add(new Card(17 ,"G" ,"Blue"  ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("n18") ));
-      cardList.Add(new Card(18 ,"B" ,"Red"   ,"One" ,"Merk",1 ,Resources.Load<Sprite>("n19") ));
-      cardList.Add(new Card(19 ,"B" ,"Red"   ,"One" ,"Game",1 ,Resources.Load<Sprite>("n20") ));
-      cardList.Add(new Card(20 ,"B" ,"Red"   ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("n21") ));
-      cardList.Add(new Card(21 ,"B" ,"Green" ,"One" ,"Merk",1 ,Resources.Load<Sprite>("n22") ));
-      cardList.Add(new Card(22 ,"B" ,"Green" ,"One" ,"Game",1 ,Resources.Load<Sprite>("n23") ));
-      cardList.Add(new Card(23 ,"B" ,"Green" ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("n24") ));
-      cardList.Add(new Card(24 ,"B" ,"Blue"  ,"One" ,"Merk",1 ,Resources.Load<Sprite>("n25") ));
-      cardList.Add(new Card(25 ,"B" ,"Blue"  ,"One" ,"Game",1 ,Resources.Load<Sprite>("n26") ));
-      cardList.Add(new Card(26 ,"B" ,"Blue"  ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("n27") ));
+      cardList.Add(new Card(0  ,"R" ,"Red"   ,"One" ,"Merk",1 ,Resources.Load<Sprite>("o1") ));
+      cardList.Add(new Card(1  ,"R" ,"Red"   ,"One" ,"Game",1 ,Resources.Load<Sprite>("o2") ));
+      cardList.Add(new Card(2  ,"R" ,"Red"   ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("o3") ));
+      cardList.Add(new Card(3  ,"G" ,"Red"   ,"One" ,"Merk",1 ,Resources.Load<Sprite>("o4") ));
+      cardList.Add(new Card(4  ,"G" ,"Red"   ,"One" ,"Game",1 ,Resources.Load<Sprite>("o5") ));
+      cardList.Add(new Card(5  ,"G" ,"Red"   ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("o6") ));
+      cardList.Add(new Card(6  ,"B" ,"Red"   ,"One" ,"Merk",1 ,Resources.Load<Sprite>("o7") ));
+      cardList.Add(new Card(7  ,"B" ,"Red"   ,"One" ,"Game",1 ,Resources.Load<Sprite>("o8") ));
+      cardList.Add(new Card(8  ,"B" ,"Red"   ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("o9") ));
+
+      cardList.Add(new Card(9  ,"R" ,"Green" ,"One" ,"Merk",1 ,Resources.Load<Sprite>("o10") ));
+      cardList.Add(new Card(10 ,"R" ,"Green" ,"One" ,"Game",1 ,Resources.Load<Sprite>("o11") ));
+      cardList.Add(new Card(11 ,"R" ,"Green" ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("o12") ));
+      cardList.Add(new Card(12 ,"G" ,"Green" ,"One" ,"Merk",1 ,Resources.Load<Sprite>("o13") ));
+      cardList.Add(new Card(13 ,"G" ,"Green" ,"One" ,"Game",1 ,Resources.Load<Sprite>("o14") ));
+      cardList.Add(new Card(14 ,"G" ,"Green" ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("o15") ));
+      cardList.Add(new Card(15 ,"B" ,"Green" ,"One" ,"Merk",1 ,Resources.Load<Sprite>("o16") ));
+      cardList.Add(new Card(16 ,"B" ,"Green" ,"One" ,"Game",1 ,Resources.Load<Sprite>("o17") ));
+      cardList.Add(new Card(17 ,"B" ,"Green" ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("o18") ));
+
+      cardList.Add(new Card(18 ,"R" ,"Blue"  ,"One" ,"Merk",1 ,Resources.Load<Sprite>("o19") ));
+      cardList.Add(new Card(19 ,"R" ,"Blue"  ,"One" ,"Game",1 ,Resources.Load<Sprite>("o20") ));
+      cardList.Add(new Card(20 ,"R" ,"Blue"  ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("o21") ));
+      cardList.Add(new Card(21 ,"G" ,"Blue"  ,"One" ,"Merk",1 ,Resources.Load<Sprite>("o22") ));
+      cardList.Add(new Card(22 ,"G" ,"Blue"  ,"One" ,"Game",1 ,Resources.Load<Sprite>("o23") ));
+      cardList.Add(new Card(23 ,"G" ,"Blue"  ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("o24") ));
+      cardList.Add(new Card(24 ,"B" ,"Blue"  ,"One" ,"Merk",1 ,Resources.Load<Sprite>("o25") ));
+      cardList.Add(new Card(25 ,"B" ,"Blue"  ,"One" ,"Game",1 ,Resources.Load<Sprite>("o26") ));
+      cardList.Add(new Card(26 ,"B" ,"Blue"  ,"One" ,"Tic" ,1 ,Resources.Load<Sprite>("o27") ));
     
     //Debug.Log("CardList Count after adding: " + cardList.Count);
 
